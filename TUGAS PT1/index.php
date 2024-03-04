0effe74795633bc01228402ef90c10ffffa3f238
gabriellesmana <mamypoko821@gmail.com>
2024-03-04 19:19:49 +0700

    first commit

---
+++ TUGAS PT1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+print "<pre>";
+print "<center>";
+print "<tr><td><font size = 16 color = 'red'><strong>BIODATAKU </strong></font></td></tr>";
+print "<br><br>";
+print "<table align='center'>";
+print "<tr><td><font size = 12 color='blue'>Nama        : <strong>CERDAS G LESMANA</strong></font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Email       : cerdaslesmana.100@gmail.com</font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Jurusan     : Teknik Informatika</font></td></tr>";
+print "<tr><td><font size = 12 color='blue'>Hobby       : Gaming</font></td></tr>";
+print "</table>";
+print "</center>";
+?>
+</body>
+</html>
