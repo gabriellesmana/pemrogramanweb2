7a02e7f6c9134fc9911ace8b357014d640bc875f
gabriellesmana <mamypoko821@gmail.com>
2024-03-05 19:23:17 +0700

    perbarui folder

---
+++ TUGAS PT1/SOAL 2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Belajar PHP</title>
+</head>
+<body>
+   <h1>Selamat datang ke PHP</h1>
+   <p>Ini adalah kesan pertama saya belajar PHP.</p>
+
+   <?php
+   
+   echo "<p>Hello, World!</p>";
+
+   
+   $nama = "Cerdas G Lesmana";
+   $kesan = "saya senang belajar PHP";
+   echo "$kesan";
+   echo "<p>Nama saya adalah: $nama</p>";
+
+
+   ?>
+</body>
+</html>
